43f9e06604f3d91a24b6a568d1ea108f4e522c6c
ma44ass <assma.boutezrout@gmail.com>
2025-02-22 22:59:21 +0100

    Home page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import { LoadingScreen } from './components/LoadingScreen';
 import { Navbar } from './components/Navbar';
 import { MobileMenu } from './components/MobileMenu';
+import { Home } from './components/sections/Home';
 import "./index.css";
 
 function App() {
@@ -18,6 +19,8 @@ function App() {
     <div className={`min-h-screen transition-opacity duration-700 ${isLoaded ? "opacity-100" : "opacity-0"} bg-black text-gray-100`}>
       <Navbar menuOpen ={menuOpen} setMenuOpen={setMenuOpen} />
       <MobileMenu menuOpen ={menuOpen} setMenuOpen={setMenuOpen} />
+      <Home />
+      
     </div>
   </>
   );
