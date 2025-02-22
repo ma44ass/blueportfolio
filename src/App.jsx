b43fc4ea806fa700cb60d614ea534f50e1443e2c
ma44ass <assma.boutezrout@gmail.com>
2025-02-22 20:47:43 +0100

    Responsive Nav Bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,26 @@
 import './App.css';
 import { useState } from 'react';
 import { LoadingScreen } from './components/LoadingScreen';
+import { Navbar } from './components/Navbar';
+import { MobileMenu } from './components/MobileMenu';
 import "./index.css";
 
 function App() {
 {/*cheching if the loading screen is still shown*/}
   const [isLoaded, setIsLoaded]= useState(false)
+{/*cheching if the Menu is open*/}
+  const [menuOpen, setMenuOpen]= useState(false)
 
   return (
   <>
-      {!isLoaded && <LoadingScreen onComplete={()=> setIsLoaded(true)}/>}
+    {!isLoaded && <LoadingScreen onComplete={()=> setIsLoaded(true)}/>}{""}
 
-  </>);
+    <div className={`min-h-screen transition-opacity duration-700 ${isLoaded ? "opacity-100" : "opacity-0"} bg-black text-gray-100`}>
+      <Navbar menuOpen ={menuOpen} setMenuOpen={setMenuOpen} />
+      <MobileMenu menuOpen ={menuOpen} setMenuOpen={setMenuOpen} />
+    </div>
+  </>
+  );
 }
 
 export default App;
