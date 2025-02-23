4fe47a046b8c11984a5eb73172a67ee5f6e05d3c
ma44ass <assma.boutezrout@gmail.com>
2025-02-23 20:56:21 +0000

    contact section

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import { Home } from './components/sections/Home';
 import { About } from './components/sections/About';
 import { Projects } from './components/sections/Projects';
 import "./index.css";
+import { Contact } from './components/sections/Contact';
 
 function App() {
 {/*cheching if the loading screen is still shown*/}
@@ -24,6 +25,7 @@ function App() {
       <Home />
       <About />
       <Projects />
+      <Contact />
     </div>
   </>
   );
